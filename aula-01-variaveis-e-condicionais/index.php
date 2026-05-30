c7d6921cb2222f6b397a9c4a8d925963302cfcff
Marcos Santos <102172111+marcossantos1977@users.noreply.github.com>
2026-05-29 23:47:09 -0300

    Adiciona exercício de variáveis e condicionais

---
+++ aula-01-variaveis-e-condicionais/index.php
@@ -0,0 +1,36 @@
+<?php
+
+$nome = "Marcos";
+$idade = 17;
+$cidade = "Fortaleza";
+$profissaoAtual = "Desenvolvedor Junior";
+$objetivo = "Desenvolvedor Junior em PHP/Laravel";
+$nota = 4;
+
+echo "<h1>Meu primeiro programa PHP</h1>";
+
+echo "Nome: " . $nome;
+echo "<br>";
+
+echo "Idade: " . $idade;
+echo "<br>";
+
+echo "Cidade: " . $cidade;
+echo "<br>";
+
+echo "Profissão Atual: " . $profissaoAtual;
+echo "<br>";
+
+echo "Objetivo: " . $objetivo;
+
+echo "<br><br>";
+
+if ($nota >= 7) {
+    echo "Resultado: Aprovado.";
+
+} elseif ($nota >= 5 && $nota < 7) {
+    echo "Resultado: Recuperação";
+    
+} else {
+    echo "Resultado: Reprovado.";
+}
